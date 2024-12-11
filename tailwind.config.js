e5f34bd3750cb20cccdb4353dbebe69757b5d7aa
Lucas-cazita <lucascazita@gmail.com>
2024-12-11 07:02:12 +0000

    trabalhando com Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./fundamentos/src/**/*.{html,js,ts}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        vermelho: '#F00'
+    }
+    },
+  },
+  plugins: [],
+}
+
